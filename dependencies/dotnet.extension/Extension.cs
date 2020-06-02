9f90f77cfadaa91b0ef4cfcd123407b6d55a193f
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2020-06-02 13:17:13 -0700

    v1.0.1

--- dependencies/dotnet.extension/Extension.cs
+++ dependencies/dotnet.extension/Extension.cs
@@ -6,77 +6,46 @@ namespace RUDD.Dotnet
 {
     public class Util
     {
-        public static DataStore data;
-        public static string[] players = new string[256];
-        public const string ROSTER = "ROSTER", KEY = "PLAYERS";
-        private static bool once;
-        protected static void Initialize()
-        {
-            data = new DataStore("config\\util_player_roster");
-            if (!data.BlockExists(ROSTER))
-            {
-                data.NewBlock(new string[] {KEY}, ROSTER);
-            }
-        }
-        public static void AddPlayer(string Name)
-        {
-            if (!once)
-            {
-                Initialize();
-                once = true;
-            }
-            Block roster = data.GetBlock(ROSTER);
-            string list = roster.GetValue(KEY);
-            string userName = Name;
-            if (!list.Contains(";"))
-            {
-                roster.WriteValue(KEY, userName + ";");
-                return;
-            }
-            for (int i = 0; i < list.Length; i++)
-            {
-                if (list.Substring(i).StartsWith(userName))
-                    return;
-            }
-            roster.AddValue(KEY, ';', userName);
-        }
         public static TSPlayer FindPlayer(string Name)
         {
-            if (!once)   
+            int[] index = new int[256];
+            for (int i = 0; i < index.Length; i++)
             {
-                Initialize();
-                once = true;
+                index[i] = 0;
             }
-            Block roster = data.GetBlock(ROSTER);
-            string list = roster.GetValue(KEY).Replace(';', ' ') + " ..";
-            Console.WriteLine(list);
             foreach(TSPlayer p in TShock.Players)
             {
                 if (p != null && p.Active)
                 {
-                    for (int i = 0; i < list.Length - 3; i++)
+                    index[p.Index] = NameMatch(p.Name.ToLower(), Name.ToLower());
+                }
+            }
+            int closestMatch = -1;
+            for (int i = 0; i < index.Length -1; i++)
+            {
+                if (index[i] > index[i + 1])
+                    closestMatch = i;
+            }
+            return closestMatch == -1 ? null : TShock.Players[closestMatch];
+        }
+        private static int NameMatch(string input, string search)
+        {
+            int count = 0;
+            if (input.StartsWith(search.Substring(0, 1)))
+            {
+                count++;
+                for (int i = 1; i < input.Length; i++)
+                {
+                    if (i < search.Length)
                     {
-                        string sub;
-                        string sub2;
-                        if ((sub = list.Substring(i, 2).ToLower()) == (sub2 = p.Name.ToLower().Substring(0, 2)))
+                        if (input.Substring(i, 1) == search.Substring(i, 1))
                         {
-                            Console.WriteLine(list.Substring(i, 2).ToLower());
-                            Console.WriteLine(p.Name.ToLower().Substring(0, 2));
-                            if (sub == sub2)
-                            {
-                                if (list.Substring(i).ToLower().StartsWith(p.Name.ToLower()))
-                                Console.WriteLine("success");
-                                return p;
-                            }
+                            count++;
                         }
                     }
                 }
             }
-            return null;
-        }
-        public static void WriteFile()
-        {
-            data.WriteToFile();
+            return count;  
         }
     }
 }
